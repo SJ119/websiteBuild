83b3ad67044b5ee33b9b9e44832f47132b83c5a0
Shen Jin <shen_jin@live.com>
2015-08-18 20:40:16 -0700

    Added normalize css

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,11 +10,19 @@ module.exports = function(grunt) {
 			files: ['gruntfile.js', 'src/**/*.js']
 		},
 		copy: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: 'dist/',
+					src: ['**/*'],
+					dest: '../websiteDist/'
+				}]
+			},
 			lib: {
 				files: [{
 					expand: true,
 					cwd: 'bower_components/',
-					src: ['angular/angular*.js', 'angular-animate/angular-animate*.js', 'd3/d3*.js', 'd3-tip/index.js', 'jQuery/dist/jquery*.js', 'bootstrap/dist/css/bootstrap.min.css'],
+					src: ['angular/angular*.js', 'angular-animate/angular-animate*.js', 'd3/d3*.js', 'd3-tip/index.js', 'jQuery/dist/jquery*.js', 'bootstrap/dist/css/bootstrap.min.css', 'normalize.css/normalize.css'],
 					dest: 'dist/lib/'
 				}]
 			},
@@ -84,14 +92,16 @@ module.exports = function(grunt) {
 		},
 		watch: {
 			files: ['<%= jshint.files %>', 'src/**/*.less', 'src/**/*.html'],
-			tasks: ['jshint', 'concat', 'less', 'cssmin', 'uglify', 'copy'],
+			tasks: ['jshint', 'concat', 'less', 'cssmin', 'uglify', 'copySrc'],
 			options: {
 				livereload: true
 			}
 		}
 	});
 
+	grunt.registerTask('copySrc', ['copy:lib', 'copy:html', 'copy:assets', 'copy:readme']);
+	grunt.registerTask('export', ['copy:dist']);
 	grunt.registerTask('gallery', ['connect', 'watch']);
-	grunt.registerTask('default', ['jshint', 'concat', 'less', 'cssmin', 'uglify', 'copy']);
+	grunt.registerTask('default', ['jshint', 'concat', 'less', 'cssmin', 'uglify', 'copySrc']);
 
 };
